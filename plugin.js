387c94ecd64ee940b455a29c86400e7f9a589333
Manfies <okunevden97@gmail.com>
2020-01-27 12:05:00 +0300

    Update for last wersion from original PR

--- plugin.js
+++ plugin.js
@@ -7,9 +7,27 @@ class GTM {
   }
   init() {
 
+    // insert scripts on client side if it not preseted on server side
     if (!this.options.presetScriptsOnServerSide) {
-      this.ctx.app.head.script.push(this.options.head.script[0])
-      this.ctx.app.head.noscript.push(this.options.head.noscript[0])
+      const headScriptId = 'google-tag-manager-script'
+      const bodyScriptId = 'google-tag-manager-noscript'
+
+      let headScript = document.querySelector(`script#${headScriptId}`)
+      let bodyScript = document.querySelector(`script#${bodyScriptId}`)
+
+      if (!headScript && !bodyScript) {
+        headScript = document.createElement('script')
+        headScript.id = headScriptId
+        headScript.src = this.options.head.script[0].src
+        headScript.async = this.options.head.script[0].async
+
+        bodyScript = document.createElement('noscript')
+        bodyScript.id = bodyScriptId
+        bodyScript.text = this.options.head.noscript[0].innerHTML
+
+        document.querySelector('head').append(headScript)
+        document.querySelector('body').append(bodyScript)
+      }
     }
 
     window[this.options.layer] = window[this.options.layer] || []
@@ -27,7 +45,13 @@ class GTM {
   initPageTracking() {
     this.ctx.app.router.afterEach((to, from) => {
       setTimeout(() => {
-        window[this.options.layer].push(to.gtm || { event: this.options.pageViewEventName, pageType: 'PageView', pageUrl: to.fullPath, routeName: to.name })
+        window[this.options.layer]
+          .push(to.gtm || {
+            routeName: to.name,
+            pageType: 'PageView',
+            pageUrl: to.fullPath,
+            event: this.options.pageViewEventName
+          })
       }, 0)
     })
   }
@@ -50,11 +74,17 @@ class GTM {
   }
 
   hasDNT() {
-    return window.doNotTrack === '1' ||
+    return (
+      window.doNotTrack === '1' ||
       navigator.doNotTrack === 'yes' ||
       navigator.doNotTrack === '1' ||
       navigator.msDoNotTrack === '1' ||
-      (window.external && window.external.msTrackingProtectionEnabled && window.external.msTrackingProtectionEnabled())
+      (
+        window.external &&
+        window.external.msTrackingProtectionEnabled &&
+        window.external.msTrackingProtectionEnabled()
+      )
+    )
   }
 }
 
